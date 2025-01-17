2e4b6dedc4ab5b19b787c7a81e7dec95ff49d621
bhisma12 <bagusbhismantara12@gmail.com>
2025-01-17 09:07:56 +0800

    secong commit

--- frontend/shop.php
+++ frontend/shop.php
@@ -14,6 +14,7 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop page</title>
+    <link rel="stylesheet" href="shop.css">
 </head>
 <body>
     
